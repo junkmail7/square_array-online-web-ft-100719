844ac7de08373b7fa18a96f024ef6dbc5710ddcb
William Pannell <lpann5@gmail.com>
2019-10-13 22:00:16 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,7 @@
 def square_array(array)
-  # your code here
+  brothers.each do |brother|
+  puts "This is loop number #{counter}"
+  puts "Stop hitting yourself #{brother}!"
+  counter += 1
+end
 end
